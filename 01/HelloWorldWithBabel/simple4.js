9bc50c4dfcf33cce3cc03842cb7ac4cb0d1f2fac
kopro-ov <koprodev@gmail.com>
2021-03-05 16:22:54 +0900

    jsx문법을 바벨로 이용하여 변환

--- 01/HelloWorldWithBabel/simple4.js
+++ 01/HelloWorldWithBabel/simple4.js
@@ -16,15 +16,13 @@ npx babel --watch src --out-dir . --presets @babel/preset-react
 */
 function Container() {
   const [count, setCount] = React.useState(0);
-  return(
-    <div>
-        <LikeButton />
-        <div style={{ marginTop: 20 }}>
-          <span>현재 카운트 : </span>
-          <span>{count }</span>
-          <button onClick={()=> setCount(count + 1)}>증가</button>
-          <button onClick={()=> setCount(count - 1)}>감소</button>
-        </div>
-    </div>
-  );
+  return /*#__PURE__*/React.createElement("div", null, /*#__PURE__*/React.createElement(LikeButton, null), /*#__PURE__*/React.createElement("div", {
+    style: {
+      marginTop: 20
+    }
+  }, /*#__PURE__*/React.createElement("span", null, "\uD604\uC7AC \uCE74\uC6B4\uD2B8 : "), /*#__PURE__*/React.createElement("span", null, count), /*#__PURE__*/React.createElement("button", {
+    onClick: () => setCount(count + 1)
+  }, "\uC99D\uAC00"), /*#__PURE__*/React.createElement("button", {
+    onClick: () => setCount(count - 1)
+  }, "\uAC10\uC18C")));
 }
