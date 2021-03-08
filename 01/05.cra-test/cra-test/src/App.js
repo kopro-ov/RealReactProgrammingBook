2e2bc92dcdeb0b4ad4d056642a90310b7284a013
kopro-ov <koprodev@gmail.com>
2021-03-08 15:52:24 +0900

    생성된 환경변수 console.log

--- 01/05.cra-test/cra-test/src/App.js
+++ 01/05.cra-test/cra-test/src/App.js
@@ -29,5 +29,7 @@ function App() {
   );
 }
 console.log(`NODE_ENV = ${process.env.NODE_ENV}`);
+console.log(`REACT_APP_DATA_API = ${process.env.REACT_APP_DATA_API}`);
+console.log(`REACT_APP_LOGIN_API = ${process.env.REACT_APP_LOGIN_API}`);
 export default App;
 
