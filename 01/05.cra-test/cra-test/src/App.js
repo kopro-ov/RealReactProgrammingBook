6786b3a8d1b6ae7f559b98ea10678a512874a242
kopro-ov <koprodev@gmail.com>
2021-03-08 15:41:50 +0900

    App.js 파일에서 TodoList 컴포넌트 사용하기

--- 01/05.cra-test/cra-test/src/App.js
+++ 01/05.cra-test/cra-test/src/App.js
@@ -2,10 +2,12 @@ import logo from './logo.svg';
 import './App.css';
 import smallImage from './small.jpeg';
 import bigImage from './big.jpeg';
+import TodoList from './TodoList'
 
 function App() {
   return (
     <div className="App">
+      <TodoList />
       <img src={bigImage} alt="big" />
       <img src={smallImage} alt="small" />
       <header className="App-header">
