d469e50cbf802861840cbddec1724e994c32787e
kopro-ov <koprodev@gmail.com>
2021-03-08 17:06:56 +0900

    React-router-dom으로 작성한 단일 페이지 애플리케이션

--- 01/07.router-test/router-test/src/App.js
+++ 01/07.router-test/router-test/src/App.js
@@ -1,43 +1,29 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
+import { BrowserRouter, Route, Link } from 'react-router-dom'
+import Rooms from './Rooms';
 
 export default function App() {
-
-  const [pageName, setPageName] = useState('');  
-  useEffect(() => {
-    window.onpopstate = event => {
-      setPageName(event.state); // popstate이벤트가 발생하면 페이지를 전환한다는 의미로 pageName 상탯값을 수정한다.
-    };
-  }, []);
-  function onClick1() {
-    const pageName ='page1';
-    window.history.pushState('v1', '', '/page1');
-    setPageName(pageName);
-  }
-  function onClick2() {
-    const pageName ='page2';
-    window.history.pushState('v2', '', '/page2');
-    setPageName(pageName);
-  }  
   return (
-    <div>
-      <button onClick={onClick1}>
-        page1
-      </button>
-      <button onClick={onClick2}>
-        page2
-      </button>
-      {!pageName && <Home />}
-      {pageName == 'page1' && <Page1 />}
-      {pageName == 'page2' && <Page2 />}
-    </div>
+    <BrowserRouter>
+      <div style={{ padding: 20, border: '5px solid gray' }}>
+        <Link to="/">홈</Link>
+        <br />
+        <Link to="/photo">사진</Link>
+        <br />
+        <Link to="/rooms">방 소개</Link>
+        <br />
+        <Route exact path="/" component={Home} />
+        <Route exact path="/photo" component={Photo} />
+        <Route exact path="/rooms" component={Rooms} />
+      </div>      
+    </BrowserRouter>
   );
 }
-function Home() {
-  return <h2>여기는 홈페이지입니다. 원하는 페이지 버튼을 클릭하세요.</h2>;
+
+function Home({ match }) {
+  return <h2>이곳은 홈페이지입니다.</h2>;
 }
-function Page1(){
-  return <h2>여기는 Page1입니다.</h2>;
-}
-function Page2() {
-  return <h2>여기는 Page2입니다.</h2>;
+
+function Photo({ match }) {
+  return <h2>여기서 사진을 감상하세요.</h2>;
 }
---
+++ 01/07.router-test/router-test/src/Rooms.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+//별도의 파일로 구현 할 예정
+function Rooms({}) {
+    return <div></div>
+}
+
+export default Rooms;
